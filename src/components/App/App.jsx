20865005b37258413bc22c54a8a0591cc5c9af76
Yevhen <leshchenkoyevhen@gmail.com>
2022-12-06 14:08:05 -0500

    add form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,36 @@
 import { Component } from 'react';
 import css from './App.module.css';
-import { getImages } from '../../services/image-api';
+import { SearchForm } from 'components/Searchbar/Searchbar';
+
+// import { getImages } from '../../services/image-api';
 
 export class App extends Component {
   state = {
     images: [],
-    image: '',
+    image: 'cat',
     isLoading: false,
+  };
+
+  handleSubmit = data => {
+    console.log(data);
+    this.setState({ image: data.image });
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState !== this.props.state) {
-      console.log(getImages(this.state.image));
+      // const data = getImages(this.state.image).then(res =>
+      //   res.json().then(res)
+      // );
+      // this.setState()
     }
   }
 
   render() {
-    return <div className={css.appWrapper}>Images</div>;
+    return (
+      <div className={css.appWrapper}>
+        Images
+        <SearchForm onFormSubmit={this.handleSubmit} />
+      </div>
+    );
   }
 }
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,38 @@
+import { Formik, Form, Field } from 'formik';
+
+export const SearchForm = ({ onFormSubmit }) => {
+  const initialValues = {
+    image: '',
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    if (values.image === '') {
+      alert('Type anything');
+      return;
+    }
+
+    onFormSubmit(values);
+    resetForm();
+  };
+
+  return (
+    <header>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <button type="submit">
+            <span>Search</span>
+          </button>
+          <Field type="text" name="image" />
+        </Form>
+      </Formik>
+    </header>
+  );
+};
+
+// <input
+//   class="input"
+//   type="text"
+//   autocomplete="off"
+//   autofocus
+//   placeholder="Search images and photos"
+// />
